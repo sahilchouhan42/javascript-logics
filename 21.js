dd8f68050b88810c43b6c240273839b6732780b1
Sahil Chouhan <chouhangolu604@gmail.com>
2026-02-02 11:53:25 +0530

    new file

--- 21.js
+++ 21.js
@@ -6,7 +6,7 @@
     for(let i=0; i<str.length; i++){
         for(let j=0; j<vowel.length; j++){
             if(str[i]==vowel[j]){
-                count ++
+                 count ++
             }
         }
     }
---
+++ 22.js
@@ -0,0 +1,16 @@
+//reverse an array
+
+let arr = [1, 2, 3, 4, 5]
+let start = 0
+let end = arr.length-1
+
+while(start<end){
+    let temp = arr[start]
+    arr[start] = arr[end]
+    arr[end] = temp
+
+    start++
+    end--
+}
+
+console.log(arr)
